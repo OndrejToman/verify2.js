69168200c7c30c63dc5ba287c061d8a1500e8c2d
Ondřej Toman <tom.ondrej@hotmail.cz>
2017-07-11 16:12:03 +0200

    Translations

--- verify2.js
+++ verify2.js
@@ -3,17 +3,22 @@ $(document).ready(function(){
   /*
   Configuration variables
   */
-  var debug_mode = false; // Debug mode will write messages to JS console (for example about types of inputs)
+  var debug_mode = true; // Debug mode will write messages to JS console (for example about types of inputs)
   var send_form = true; // After script is finished, decides if POST/GET form or not (if you want only test but dont want to actually send anything)
   var automatic_id = true; // Automatically add IDs to all forms on page (if form already have one, no ID vill be added by this scripr)
-  var wrong_input_class_name = "wrong-input"; // Class that will be added to non valid forms
+  var wrong_input_class_name = "wrong-input"; // Class that will be added to non valid inputs
 
 
   /*
   Regular expressions
   */
-  var regex_phone = /^(\+420)?[0-9]{9}$/;
-
+  var expressions = Array();
+  expressions["name"] = /^\D{1,} \D{1,}$/;
+  expressions["email"] = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)+$/;
+  expressions["phone"] = /^(\+420)?[0-9]{9}$/;
+  expressions["date"] = /^\d{1,2}.\d{1,2}.\d{4}$/;
+  expressions["text"] = /\D/;
+  expressions["number"] = /\d/;
 
   /*
     Function, that finds all <form> tags in document and adds ID to each
@@ -53,139 +58,98 @@ $(document).ready(function(){
 
     /*
     Method for input validation
+    Parameters
+      -> Value: Value (text) from input
+      -> Type: Type of input (phone, name, ...)
     Returns
       -> True: Valid
       -> False: Invalid
     */
     function validuj_input(value, type){
-      var key = "";
-      if(type == "telefon"){
-        upraveny_string = value.replace(/\s+/g, ''); // Pro zjednodušení výrazů nahradíme případné mezery v tel. čísle
+       var key = expressions[type];
 
-        if(debug_mode == true){
-          console.log('Upravené číslo je: ' + upraveny_string);
-        }
-        key = regex_phone;
-
-        if(key.test(upraveny_string)) {
-          return true;
-        }else {
-          return false;
-        }
-      }else if (type == "jmeno") {
-        key = /^\D{1,} \D{1,}$/;
-
-        if(key.test(value)) {
-          return true;
-        }else {
-          return false;
-        }
-      }else if (type == "email") {
-        key = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)+$/;
-
-        if(key.test(value)) {
-          return true;
-        }else {
-          return false;
-        }
-      }else if (type == "datum") {
-        key = /^\d{1,2}.\d{1,2}.\d{4}$/;
-
-        if(key.test(value)) {
-          return true;
-        }else {
-          return false;
-        }
-      }else if (type == "text") {
-        return true;
-      }else if (type == "soubor") {
-        // Musí být nahrán soubor
-      }else if (type == "checkbox") {
-        // Checkbox
-      }else {
-        // Není vyplněno nebo má neznámý typ, vracíme false
-        return false;
-      }
+       if(key.test(value)){
+         return true;
+       }else {
+         return false;
+       }
     }
 
     $(".form-validate").on('click',function(e){ // Při kliknutí na tlačítko s třídou form-validate
       e.preventDefault();
 
       /*
-      Pole, které obsahuje správnost jednotlivých inputů
+      Array, with records about validity of inputs
       */
       var inputs = Array();
 
       var validni = true;
       /*
-      Proměnná, který definuje, zda je formulář validní
-      Formulář je na začátku nastaven na true (validní). Pokud se během kontroly objeví chyba,
-      je nastaven na false (nevalidní) a označí se input, který chybu vyvolal.
+      Variable, which defines if form is valid or not
+      In the beginning is this variable set to true (valid). If the script discovers non-valid input,
+      this variable is changed to false (non-valid) and input will be marked by class (you can change this class in variable wrong_input_class_name)
       */
 
-      var parent_form = $(this).closest('form'); // Vybereme rodičovský formlulář
+      var parent_form = $(this).closest('form'); // Select parent form
 
-      var form_object_selector = "form#" + parent_form.attr('id') + " :input"; // Sestavíme selektor pro výběr inputů formuláře
+      var form_object_selector = "form#" + parent_form.attr('id') + " :input"; // Assembled form selector
 
-      var input_iteration = 0; // Nastavení čísla pro průchod všemi inputy a následného zanesení do pole inputs
+      var input_iteration = 0; // Iteration for inputs (for purpose of inputs array)
 
       $(form_object_selector).each(function(){
-        var input_objekt = $(this); // Nastavení objektu
+        var input_objekt = $(this); // Set object
 
-        input_objekt.removeClass(wrong_input_class_name); // Před validací odstraníme classu wrong input ze všech inputů ve formuláři
+        input_objekt.removeClass(wrong_input_class_name); // Before validation remove wrong input class
 
         /*
-        Proměnné potřebné pro práci s inputem
+        Variables needed for future work with input
         */
-        var value = input_objekt.val(); // Hodnota (obsah) inputu
-        var jmeno = input_objekt.attr('name'); // Zjištění jména inputu
-        var druh = input_objekt.data('typ'); // Zjištění o jaký druh inputu se jedná
-        var vyzadovan = false; // Zjištění, zda input musí být vyplněn (defaultně nastaveno na false -> není vyžadován)
+        var value = input_objekt.val(); // Value (text) of an input
+        var jmeno = input_objekt.attr('name'); // Name attribute of input
+        var druh = input_objekt.data('type'); // Type of input (phone, name, etc.)
+        var vyzadovan = false; // Fram start every input is set as non-required
         var slovo = ""; // Pomocná proměnné pro výpis do konzole
 
-        if(input_objekt.prop('required')){ // Zjištěné, jestli má u sebe input atribut requiered
-          vyzadovan = true; // Má u sebe napsáno reqired
+        if(input_objekt.prop('required')){ // Check if input is required or not
+          vyzadovan = true; // Is required
           slovo = " je ";
         } else {
-          vyzadovan = false; // Nemá u sebe napsáno reqired
+          vyzadovan = false; // Isn't required
           slovo = " není ";
         }
-        /*
-        Debugovací výpis jednotlivých položek formuláře (druh a jestli jsou requiered)
-        */
+
         if(debug_mode == true){
           console.log("Input " + jmeno + " je druh " + druh + " a" + slovo + "vyžadováno vyplnění.");
         }
 
         /*
-        Pokud input nemá nastavený druh, bude se považovat za validní
+        If input doesn't have data-type set, is automatically valid
         */
         if(!druh){
-
           inputs[input_iteration] = true;
-
         }else{
 
           /*
           Pokud má input nastaveno reqired a není vyplněn, bude se s
           ním počítat jako s nevalidním
+          If input is required and empty, no need to validate, it's just invalid
           */
           if(vyzadovan == true && !value){
 
             if(debug_mode == true){
-              console.log("Chyba při kontrole. Input " + jmeno + " není vyplněn");
+              console.log("Input " + jmeno + " is empty and required");
             }
 
             inputs[input_iteration] = false;
-            $(this).addClass('wrong-input');
+            $(this).addClass(wrong_input_class_name);
           }else{
             if(!validuj_input(value, druh)){
 
               if(debug_mode == true){
-                console.log("Chyba při kontrole. Input " + jmeno + " neprošel validací");
+                console.log("Input " + jmeno + " isn't valid");
               }
 
-              input_objekt.addClass('wrong-input');
+              input_objekt.addClass(wrong_input_class_name);
               inputs[input_iteration] = false;
             }else{
               inputs[input_iteration] = true;
@@ -202,17 +166,25 @@ $(document).ready(function(){
         }
       }
 
-      if(validni == true){ // Zjištění, zda je formulář validní
+      if(debug_mode == true){
+        console.log("----------------");
+      }
+
+      if(validni == true){ // Final check, if form have all inputs valid
         if(debug_mode == true){
-          console.log('Formulář ' + parent_form.attr('id') + " je validní a lze jej odeslat.");
+          console.log('Form with ID ' + parent_form.attr('id') + " is valid");
         }
         if(send_form == true){
           parent_form.submit();
         }
       }else {
         if(debug_mode == true){
-          console.log('Formulář ' + parent_form.attr('id') + " není validní.");
+          console.log('Form with ID ' + parent_form.attr('id') + " isn't valid");
         }
+      }
+
+      if(debug_mode == true){
+        console.log("----------------");
       }
     });
 });
